db028cae5238bd8b268d61c269842a7b18112bdb
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-10-05 19:50:50 -0400

    change main to server.js, update layout herf

---
+++ controllers/controllered.js
@@ -0,0 +1,123 @@
+// Dependencies
+var express = require('express');
+var path = require('path');
+var request = require('request');
+var cheerio = require('cheerio');
+var router = express.Router();
+
+// Import the Comment and Article models
+var Comment = require('../models/Comment.js');
+var Article = require('../models/Article.js');
+
+// Index Page Render
+router.get('/', function (req, res) {
+	res.redirect('/scrape');
+});
+
+// Articles Page Render, Query MongoDB, load comments
+router.get('/articles', function (req, res) {
+	Article.find().sort({
+			_id: -1
+		})
+		.populate('comments')
+		.exec(function (err, doc) {
+			if (err) {
+				console.log(err);
+			} else {
+				var hbsObject = {
+					articles: doc
+				}
+				res.render('index', hbsObject);
+			}
+		});
+});
+// Web Scrape Route
+router.get('/scrape', function (req, res) {
+	request('http://www.bbc.com/news', function (error, response, html) {
+		var $ = cheerio.load(html);
+		// error handler for  duplicate articles
+		var titlesArray = [];
+		$(".gs-c-promo-heading").each(function (i, element) {
+			var result = {};
+			result.title = $(this).children("h3").text();
+			result.link = "http://www.bbc.com" + $(this).children('h3').children('a').attr('href');
+			result.summary = $(this).children('div').text();
+			if (result.title !== "" && result.summary !== "") {
+
+				// filter duplicates within a scrape
+				if (titlesArray.indexOf(result.title) == -1) {
+					titlesArray.push(result.title);
+					article.count({
+							title: result.title
+						},
+						function (err, test) {
+							if (test == 0) {
+								var entry = new Article(result);
+								entry.save(function (err, doc) {
+									if (err) {
+										console.log(err);
+									} else {
+										console.log(doc);
+									}
+								});
+
+							}
+						});
+				} else {
+					console.log('Repeated BBC Article. Not Saved to DB.')
+				}
+
+			} else {
+				console.log('Empty Content. Not Saved to DB.')
+			}
+
+		});
+		res.redirect("/articles");
+	});
+});
+// Add a Comment Route
+router.post('/add/comment/:id', function (req, res) {
+	var articleId = req.params.id;
+	var commentAuthor = req.body.name;
+	var commentContent = req.body.comment;
+	var result = {
+		author: commentAuthor,
+		content: commentContent
+	};
+	var entry = new Comment(result);
+	entry.save(function (err, doc) {
+		if (err) {
+			console.log(err);
+		} else {
+			Article.findOneAndUpdate({
+					'_id': articleId
+				}, {
+					$push: {
+						'comments': doc._id
+					}
+				}, {
+					new: true
+				})
+				.exec(function (err, doc) {
+					if (err) {
+						console.log(err);
+					} else {
+						res.sendStatus(200);
+					}
+				});
+		}
+	});
+});
+// Delete a Comment Route
+router.post('/remove/comment/:id', function (req, res) {
+	var commentId = req.params.id;
+	Comment.findByIdAndRemove(commentId, function (err, todo) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.sendStatus(200);
+		}
+	});
+});
+// Export Router to Server.js
+module.exports = router;
